6b896f908708840d4ddecac75f88840818d213eb
goorilas <wdxz_100@hotmail.com>
2024-07-09 18:10:13 -0300

    animaçoes, cor, imagens... falra a logo..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,10 @@ import { RouterOutlet } from '@angular/router';
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent {
+  title = 'Angular_Projeto';
 
+    ngOnInit(): void {
+        AOS.init();
+        window.addEventListener('load', AOS.refresh);
+  }
 }
